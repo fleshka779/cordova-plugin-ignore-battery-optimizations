efd89331c7db7ccddab1c256ca93665365e852eb
Vladislav Kovalyov <vlad.kovaleov@gmail.com>
2017-06-07 12:47:24 +0200

    - Last

--- www/BatteryOptimizations.js
+++ www/BatteryOptimizations.js
@@ -1,11 +1,11 @@
-var BatteryOptimizations = {
-	run: function (successCallback, failureCallback) {
-		cordova.exec(successCallback, failureCallback, 'BatteryOptimizations', 'run', [true]);
-	},
-	check: function (successCallback, failureCallback) {
-		cordova.exec(successCallback, failureCallback, 'BatteryOptimizations', 'check', [true]);
-	}
+function BatteryOptimizations () {
 };
 
+BatteryOptimizations.prototype.run = function (successCallback, failureCallback) {
+	cordova.exec(successCallback, failureCallback, 'BatteryOptimizations', 'run', [true]);
+}
+BatteryOptimizations.prototype.check = function (successCallback, failureCallback) {
+	cordova.exec(successCallback, failureCallback, 'BatteryOptimizations', 'check', [true]);
+}
 
 module.exports = BatteryOptimizations;
